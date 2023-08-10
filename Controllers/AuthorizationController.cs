9701e5aab1b856f0199d72ee9c8d5d5c53fa9690
Odia Adodo <subzbelow@gmail.com>
2023-08-10 02:12:21 +0100

    Add project files.

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.ObjectModel;
+
+namespace StaticFileSecureCall.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public static readonly ReadOnlyCollection<string> AuthEmails = new List<string> { "kdonaldresources@gmail.com", "abtesting911@gmail.com" }.AsReadOnly();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+namespace StaticFileSecureCall.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("check")]
+        public IActionResult Index()
+        {
+            string message = $"Api works fine and is ready to go! :)";
+            return Ok(message);
+        }
+
+        [HttpGet("reqCurrent")]
+        public IActionResult ReqCurrent()
+        {
+            return Ok("use the same api with secret key provided to you in the body of the api",);
+            return RedirectToAction("Index","Authorization");   
+        }
+
+        [HttpGet("{fileName}")]
+        private IActionResult Download(string fileName)
+        {
+            //aws filepath
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles", fileName);
+
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                stream.CopyTo(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, GetContentType(filePath), Path.GetFileName(filePath));
+        }
+
+        private string GetContentType(string path)
+        {
+            var provider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(path, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
+        }
+    }
+}
+
