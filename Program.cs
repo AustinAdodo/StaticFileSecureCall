7d178e9c769699ec9d606082cb13af61dcc3c5a5
Odia Adodo <subzbelow@gmail.com>
2023-08-18 15:20:00 +0100

    Added Globals file for all namspaces

--- Program.cs
+++ Program.cs
@@ -1,28 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
-using Amazon.Extensions.NETCore.Setup;
-using Microsoft.AspNetCore.Hosting;
-using Amazon.SimpleEmail;
-using System.IO;
-using Amazon;
-using Amazon.SecretsManager;
-using Amazon.SecretsManager.Model;
-using Microsoft.Extensions.Configuration;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
-using StaticFileSecureCall;
-using StaticFileSecureCall.DataManagement;
-using StaticFileSecureCall.Services;
-using System;
-using AspNetCoreRateLimit;
-using Microsoft.Extensions.Hosting;
-using System.Text.Json;
-using Microsoft.Extensions.Options;
-using System.ComponentModel.Design;
-using System.Data.Entity;
-using Microsoft.Extensions.FileProviders;
-using System.Security;
-using Microsoft.Data.SqlClient;
-
 internal class Program
 {
     /// <summary>
